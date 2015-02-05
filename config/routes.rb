8c93e7a510f9296dd7636bc277c1c7d0ab1f23b1
nelltaylor <hj.nell.taylor@gmail.com>
2015-02-05 15:23:03 -0600

    began sessions models and controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :comments
   resources :tags
 
+  get "/log-in" => "sessions#new"
+  post "/log-in" => "sessions#create"
+  get "/log-out" => "sessions#destroy", as: :log_out
+
   root 'posts#index'
 
   resources :posts
