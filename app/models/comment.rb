e72a03d464613a38689298b8d84ef590f8cc6d9c
Glenna Mowry <glennamowry@gmail.com>
2015-02-05 16:47:37 -0600

    added associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,9 @@
 class Comment <ActiveRecord::Base
-acts_as_votable
 
-belongs_to :commenter, class_name: "User"
-belongs_to :post
-has_many :replies
+  acts_as_votable
+
+  belongs_to :commenter, class_name: "User"
+  belongs_to :post
+  has_many :replies
+
 end
