13ba702f5bf18db477e181a3cee39ead98336009
Dalal Alrayes <dalrayes@gmail.com>
2015-02-05 15:28:00 -0600

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,34 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-30.times do 
+20.times do
 	Post.new(title: Faker::Hacker.say_something_smart,
 					image_url: 'http://lorempixel.com/300/200/animals/',
-					author_id: rand(1..50))
+					author_id: rand(1..20))
 end
 
-25.times do
+20.times do
 	User.new(username: Faker::Internet.user_name,
            email: Faker::Internet.email,
            password: Faker::Internet.password)
 end
 
-25.times do
+20.times do
 	Vote.new(votable_id: 1, votable_type: Post )
+end
+
+20.times do
+	Post_tag.new(post_id:rand(1..20), tag_id:rand(1..20))
+end
+
+20.times do
+	Comment.new(text: Faker::Hacker.say_something_smart, commenter_id: rand(1..20), post_id: rand(1..20))
+end
+
+20.times do
+	Replies.new(text: Faker::Hacker.say_something_smart, replier_id: rand(1..20), comment_id: rand(1..20))
+end
+
+20.times do
+	Tags.new(name: Faker::Hacker.noun)
 end
