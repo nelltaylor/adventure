f2beb211d4e4b741be1bbb872a9fa71c3d037deb
Glenna Mowry <glennamowry@gmail.com>
2015-02-05 16:47:22 -0600

    updated controller methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,27 @@
 class CommentsController < ApplicationController
 
 	def index
+    @comments = Comment.all
+    #change this to be specific to the post
 	end
 
+  def new
+    @comment = Comment.new
+  end
+
   def create
-    post = Post.find(params[:id])
-    @comment = Comment.new(text: params[:text], post_id: post.id, commenter_id: session[:user_id])
-    @comment.save
-    #redirect_to post_path(@post)
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      #what if comment fails?
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text, :commenter_id)
   end
 
 end
