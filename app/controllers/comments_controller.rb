953db9775b1fc56cc70b485748ba86276f5fb883
Glenna Mowry <glennamowry@gmail.com>
2015-02-05 16:23:10 -0600

    commentscontroller adding methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,13 @@ class CommentsController < ApplicationController
 	def index
   end
 
+  def new
+  end
+
   def create
-    @comment = Comment.create(text: params[:text], commenter_id: @current_user.id)
-    redirect_to post_path(@post)
+    @comment = Comment.new(text: params[:text], post_id: post.id, commenter_id: session[:user_id])
+    @comment.save
+    #redirect_to post_path(@post)
   end
 
 end
