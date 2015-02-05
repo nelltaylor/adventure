f28bb169090f95e24caf6f6271d95f399dd9c861
nelltaylor <hj.nell.taylor@gmail.com>
2015-02-05 15:27:46 -0600

    Added user controller and model bcrypt details, and user signup view.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,17 @@ class User < ActiveRecord::Base
 	has_many :posts, foreign_key: :author_id
 	has_many :comments, foreign_key: :commenter_id
 	has_many :replies, foreign_key: :replier_id
+
+  def password
+    BCrypt::Password.new(self.password_hash)
+  end
+
+  def password=(input)
+    self.password_hash = BCrypt::Password.create(input)
+  end
+
+  def self.authenticate(email, password_attempt)
+    user = User.find_by(email: email)
+    user if user.password == password_attempt
+  end
 end
