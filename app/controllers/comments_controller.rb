2b9968bca9ce533c1828698a3753aa659c2e8176
Glenna Mowry <glennamowry@gmail.com>
2015-02-05 16:17:58 -0600

    minor edits

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = Comment.create(text: params[:text], commenter_id: params[:user_id])
+    @comment = Comment.create(text: params[:text], commenter_id: @current_user.id)
     redirect_to post_path(@post)
   end
 
