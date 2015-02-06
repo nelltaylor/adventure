f3a83b4cbab53a43db5ee8eb764682f83acaa1a2
Glenna Mowry <glennamowry@gmail.com>
2015-02-06 14:26:49 -0600

    added vote moethods to reply model and controller

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -2,5 +2,10 @@ class Reply < ActiveRecord::Base
 
 	belongs_to :comment
 	belongs_to :replier, class_name: 'User'
+  has_many :votes, :as => :votable
+
+  def total_votes
+    self.votes.where(vote:true).count - self.votes.where(vote:false).count
+  end
 
 end
