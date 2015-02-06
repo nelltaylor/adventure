68b604f78e3150d84368f72272bbfa4dcf1df4a5
Glenna Mowry <glennamowry@gmail.com>
2015-02-06 14:26:21 -0600

    added vote methods to comment model and controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,17 +13,18 @@ class CommentsController < ApplicationController
   end
 
   def voteup
-    @comment = Comment.find(params[:id])
-
-    # @comment.upvote_by current_user
-    redirect_to action: "show", controller: 'posts'
+    puts params
+    post = Post.find(params[:id])
+    @comment = Comment.find(params[:name])
+    Vote.create(vote: true, votable: @comment, voter_id: current_user.id, vote_weight: 1)
+    redirect_to post_path(post)
   end
 
   def votedown
-     @comment = Comment.find(params[:id])
-#
-    # @comment.downvote_by current_user
-    redirect_to action: "show", controller: 'posts'
+    post = Post.find(params[:id])
+    @comment = Comment.find(params[:id])
+    Vote.create(vote: false, votable: @comment, voter_id: current_user.id, vote_weight: 1)
+    redirect_to post_path(post)
   end
 
   private
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,15 @@ class Comment <ActiveRecord::Base
   belongs_to :commenter, class_name: "User"
   belongs_to :post
   has_many :replies
+  has_many :votes, :as => :votable
+
+  def total_votes
+    self.votes.where(vote:true).count - self.votes.where(vote:false).count
+  end
 
 end
+
+
+
+
+
