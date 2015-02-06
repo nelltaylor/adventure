17731790f222b7e44e2c7ce4ded0729ba405db9f
Glenna Mowry <glennamowry@gmail.com>
2015-02-06 14:27:42 -0600

    added vote model to posts model and controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,10 @@ class Post < ActiveRecord::Base
 	has_many :comments
 	has_many :post_tags
 	has_many :tags, through: :post_tags
+  has_many :votes, :as => :votable
+
+  def total_votes
+    self.votes.where(vote:true).count - self.votes.where(vote:false).count
+  end
+
 end
