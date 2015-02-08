45e30cda266b75e0fff51d6cbcaa14f6e0336cfc
Glenna Mowry <glennamowry@gmail.com>
2015-02-07 21:08:27 -0600

    user controller spec done except for my_animaux route

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+require 'spec_helper'
+
+describe CommentsController do
+
+  describe
+  end
+
+  describe
+  end
+
+end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,28 +1,34 @@
 require 'rails_helper'
+require 'spec_helper'
 
 describe UsersController do
-  #   context "User signed in" do
-  #   let(:user) { FactoryGirl.create(:user) }
-  #   before do
-  #     sign_in_as user
-  #     get :index
-  #   end
-  #   it { should redirect_to(user_path(user)) }
-  # end
-
-  # context "signed out" do
-  #   before do
-  #     sign_out
-  #     get :index
-  #   end
-  #   it { should redirect_to(root_url) }
-  # end
-
-
   describe "GET #index" do
     it "renders the :index view" do
       get :index
       expect(response).to render_template :index
+    end
+  end
+
+  # describe "GET#my_animaux" do
+  #   let(:user){FactoryGirl.create(:user)}
+  #   it "shows the users account page" do
+  #     get :my_animaux
+  #   expect(response).to render_template('/posts/index')
+  #   end
+  # end
+
+  describe "GET#new" do
+    it "shows the sign up page for new users" do
+      get :new
+      expect(response).to render_template :'users/_new'
+    end
+  end
+
+  describe "POST#create" do
+    let(:user) {FactoryGirl.create(:user)}
+    it "creates a new user" do
+      post :create, user: user.attributes
+      expect(response).to redirect_to(root_path(user))
     end
   end
 
