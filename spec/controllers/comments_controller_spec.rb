80c16fc2ce257e506408a75fe7d0c3f705757603
Glenna Mowry <glennamowry@gmail.com>
2015-02-09 07:11:42 -0600

    conflicts resolved

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,1 +1,18 @@
+require 'rails_helper'
+require 'spec_helper'
 
+describe CommentsController do
+
+# Passes when line 11 "render partial: "new"" is added into Comments Controller in the new method, but that makes the partial not render in the actual website
+    describe "GET#new" do
+      let(:comment) {FactoryGirl.create(:comment)}
+      it "renders a partial for new comments" do
+        get :new, comment: comment.attributes
+        expect(response).to render_template :'comments/_new'
+      end
+    end
+
+    describe do
+    end
+
+end
